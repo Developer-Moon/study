41fb57826588398e45b6f9bae8954891961e35ee
Moon <wlsl9616@naver.com>
2022-08-09 14:09:34 +0900

    12

--- ML/m27_pca_mnist.py
+++ ML/m27_pca_mnist.py
@@ -1,35 +1,61 @@
-from unittest import result
-from sklearn.model_selection import train_test_split
-from sklearn.datasets import load_breast_cancer
-from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
-from xgboost import XGBRegressor, XGBClassifier
-import warnings
-warnings.filterwarnings(action='ignore')
+from sklearn.decomposition import PCA
+from keras.datasets import mnist
 import numpy as np
-#----------------------------------------------------------------------------------------------------------------#
-from sklearn.decomposition import PCA  # decomposition 분해
-#----------------------------------------------------------------------------------------------------------------#
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
+
+# PCA를 통해 0.95 이상인 n_composition은 몇개?
+# 0.95
+# 0.99
+# 0.999
+# 1.0
+# 힌트 : np.argmax
+
+(x_train, _), (x_test, _) = mnist.load_data() # _ = 안 들고오겠다
+
+print(x_train.shape, x_test.shape) # (60000, 28, 28) (10000, 28, 28)
+
+x = np.append(x_train, x_test, axis=0)
+print(x.shape) # (70000, 28, 28)
 
 
-#1. 데이터.
-datasets = load_breast_cancer()
-x = datasets.data
-y = datasets.target
-print(x.shape, y.shape)   # (569, 30) (569,)
 
-pca = PCA(n_components=30) # PCA 주성분 분석 - 차원축소(압축) : 열 피쳐
+x = x.reshape(70000, 28*28) # (70000, 784)
+print(x.shape)
+
+pca = PCA(n_components=784)
 x = pca.fit_transform(x)
-print(x.shape)            # (569, 10)
+
+pca_EVR = pca.explained_variance_ratio_
+
+cumsum =np.cumsum(pca_EVR)
+print(cumsum)
+
+print(np.argmax(cumsum >= 0.95) + 1)  # 154
+print(np.argmax(cumsum >= 0.99) + 1)  # 331
+print(np.argmax(cumsum >= 0.999) + 1) # 486
+print(np.argmax(cumsum >= 1.0) + 1)   # 713
 
 
+
+
+
+
+
+
+
+
+
+'''
+x = x.reshape(70000, 28*28) # (70000, 784)
+
+pca = PCA(n_components=154)
+x = pca.fit_transform(x)
+print(x.shape)
 
 pca_EVR = pca.explained_variance_ratio_ # 변환한 값의 중요도
 print(pca_EVR)
-'''
-[8.05823175e-01 1.63051968e-01 2.13486092e-02 6.95699061e-03
- 1.29995193e-03 7.27220158e-04 4.19044539e-04 2.48538539e-04
- 8.53912023e-05 3.08071548e-05 6.65623182e-06 1.56778461e-06]
-'''
+
 print(sum(pca_EVR)) # 0.9999999203185791 상위 전체의 합 : 1에 가깝다
 
 cumsum = np.cumsum(pca_EVR) # cumsum - 누적합 하나씩 더해가는걸 보여준다
@@ -40,31 +66,6 @@ plt.plot(cumsum)
 plt.grid()
 plt.show()
 
+'''
 
 
-
-
-
-
-
-"""
-x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8, random_state=123)
-
-
-#2. 모델구성
-model = RandomForestRegressor()
-
-
-#3. 컴파일, 훈련
-model.fit(x_train, y_train) # eval_metric='error'
-
-
-#4. 평가, 예측
-results = model.score(x_test, y_test)
-print('결과 :', results)
-
-# 결과 : 0.7757367060749819
-
-# n_components=11
-# 결과 : 0.7896181665270263
-"""
---
+++ ML/m27_pca_mnist2_01_DNN.py
@@ -0,0 +1,122 @@
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dense, Dropout
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score, accuracy_score 
+from sklearn.decomposition import PCA
+from keras.datasets import mnist
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+import time
+from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
+
+
+
+
+
+# 전 4가지 모델사용
+# 784개 DNN으로 만든거(최상의 성능인거 // 0.996이상)과 비교
+# time 체크 / fit에서 하고
+
+#1. 나의 최고의 DNN
+# time = ??
+# acc  = ??
+
+#2. 나의 최고의 CNN
+# time = ??
+# acc  = ??
+
+
+
+#3. PCA 0.95
+# time = ??
+# acc  = ??
+
+#4. PCA 0.99
+# time = ??
+# acc  = ??
+
+#5. PCA 0.999
+# time = ??
+# acc  = ??
+
+#6. PCA 1.0
+# time = ??
+# acc  = ??
+
+
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data() # _ = 안 들고오겠다
+
+x = np.append(x_train, x_test, axis=0)
+print(x.shape) # (70000, 28, 28)
+
+
+
+x = x.reshape(70000, 28*28) # (70000, 784)
+print(x.shape)
+
+pca = PCA(n_components=403)
+x = pca.fit_transform(x)
+
+pca_EVR = pca.explained_variance_ratio_
+
+cumsum =np.cumsum(pca_EVR)
+# print(cumsum)
+# print(np.argmax(cumsum >= 0.996) + 1)  
+
+x_train = x[:60000] # (60000, 403)
+x_test = x[60000:]  # (10000, 403)
+
+y_train = pd.get_dummies(y_train)
+y_test = pd.get_dummies(y_test)
+print(pd.get_dummies(y_train))
+print(y_train.shape) # (60000, 10)  
+
+
+
+#2. 모델구성
+model = Sequential()
+
+
+# model.add(Dense(64, input_shape=(28*28, ))) - 28x28 사이즈였다는걸 이런식으로 명시도 가능 
+model.add(Dense(64, input_shape=(403, )))
+model.add(Dense(128, activation='relu'))
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.3))
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(10, activation='softmax'))
+
+
+#3. 컴파일, 훈련
+start_time = time.time()
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy']) 
+from tensorflow.python.keras.callbacks import EarlyStopping, ModelCheckpoint      
+earlyStopping = EarlyStopping(monitor='val_loss', patience=15, mode='min', verbose=1, restore_best_weights=True)        
+
+  
+hist = model.fit(x_train, y_train, epochs=100, batch_size=100, validation_split=0.2, callbacks=[earlyStopping], verbose=1)  
+                                            # batch_size:32 디폴트값 3번정도 말 한듯
+end_time = time.time() - start_time
+
+
+#4. 결과, 예측
+results = model.evaluate(x_test, y_test)
+print('loss : ', results[0]) 
+print('accuracy : ', results[1]) 
+
+y_predict = model.predict(x_test)
+y_predict = tf.argmax(y_predict, axis=1)
+print(y_predict)
+
+y_test = tf.argmax(y_test, axis=1)
+print(y_test)
+
+acc = accuracy_score(y_test, y_predict)  
+print('accuracy :', acc)
+
+
+# acc스코어 : 0.9686
+
