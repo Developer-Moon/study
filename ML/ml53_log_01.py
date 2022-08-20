75e1c62c731046ed6fdd2130aef3a04f85eeabe5
Moon <wlsl9616@naver.com>
2022-08-20 23:49:09 +0900

    Update ml53_log_01.py

--- ML/ml53_log_01.py
+++ ML/ml53_log_01.py
@@ -13,7 +13,6 @@ from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 
 
 from sklearn.metrics import r2_score, accuracy_score
-
 import matplotlib.pyplot as plt
 
 # 데이터에따라 로그변환이 스케일러보다 좋을때가 있다
