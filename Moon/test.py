0fa54a7ce28e3d558001042e99064055d96649ee
Moon <wlsl9616@naver.com>
2022-10-28 18:11:30 +0900

    gitignore

--- Moon/test.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import pandas_profiling
-
-path = './_data/kaggle_titanic/'
-train_set = pd.read_csv(path + 'train.csv')
-
-profile = train_set.profile_report()
-print(profile)
-profile.to_file(output_file="test.html")
